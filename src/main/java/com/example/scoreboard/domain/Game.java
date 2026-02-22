2748d063fb0202d6fa31c61995beb3e3fb8abc5a
kat28bot <katarzyna.oleksiewicz28@gmail.com>
2026-02-23 00:53:18 +0100

    Game implementation

---
+++ src/main/java/com/example/scoreboard/domain/Game.java
@@ -0,0 +1,37 @@
+package com.example.scoreboard.domain;
+
+import jakarta.annotation.Nonnull;
+import lombok.Getter;
+import lombok.NonNull;
+
+@Getter
+public class Game {
+    @NonNull
+    private final Team homeTeam;
+    @NonNull
+    private final Team awayTeam;
+    private final long startOrder;
+    private Score score;
+
+    public Game(@Nonnull Team homeTeam, @Nonnull Team awayTeam, long startOrder) {
+        if (homeTeam.equals(awayTeam)) {
+            throw new IllegalArgumentException("Home and Away teams cannot be the same");
+        }
+        this.homeTeam = homeTeam;
+        this.awayTeam = awayTeam;
+        this.startOrder = startOrder;
+        this.score = new Score(0,0);
+    }
+
+    public void updateScore(int homeScore, int awayScore) {
+        this.score = new Score(homeScore, awayScore);
+    }
+
+    public int totalScore() {
+        return score.total();
+    }
+    @Override
+    public String toString() {
+        return homeTeam + " " + score.getHome() + "-" + score.getAway() + " " + awayTeam;
+    }
+}
